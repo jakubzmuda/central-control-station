f710dcce4cccdb91f64feacfb6fb0fe1299b9a3c
Jakub Zmuda <jakub.zmuda@snowflake.com>
2024-11-10 15:55:47 +0100

    dummy endpoint and test

--- backend/src/main/java/com/github/jakubzmuda/centralControlStation/investments/api/DividendsEndpoint.java
+++ backend/src/main/java/com/github/jakubzmuda/centralControlStation/investments/api/DividendsEndpoint.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class InvestmentsController {
+public class DividendsEndpoint {
 
     @GetMapping("/")
     public String index() {
