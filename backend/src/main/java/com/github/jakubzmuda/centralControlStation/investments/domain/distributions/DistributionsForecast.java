c576a145b265ebdb9f1f7ae1a2044c7793ec9718
Jakub Zmuda <inbox.jakub.zmuda@gmail.com>
2024-11-24 09:17:34 +0100

    converting class to record

--- backend/src/main/java/com/github/jakubzmuda/centralControlStation/investments/domain/distributions/DistributionsForecast.java
+++ backend/src/main/java/com/github/jakubzmuda/centralControlStation/investments/domain/distributions/DistributionsForecast.java
@@ -1,35 +1,4 @@
 package com.github.jakubzmuda.centralControlStation.investments.domain.distributions;
 
-import java.util.Objects;
-
-public class DistributionsForecast {
-    private YearlyForecast yearlyForecast;
-
-    public DistributionsForecast(YearlyForecast yearlyForecast) {
-        this.yearlyForecast = yearlyForecast;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        DistributionsForecast that = (DistributionsForecast) o;
-        return Objects.equals(yearlyForecast, that.yearlyForecast);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(yearlyForecast);
-    }
-
-    @Override
-    public String toString() {
-        return "DistributionForecast{" +
-                "yearlyForecast=" + yearlyForecast +
-                '}';
-    }
-
-    public YearlyForecast yearlyForecast() {
-        return yearlyForecast;
-    }
+public record DistributionsForecast(YearlyForecast yearlyForecast) {
 }
