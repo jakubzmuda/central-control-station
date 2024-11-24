a5339275d2dea2da5614f58a6f3fb5d743763006
Jakub Zmuda <inbox.jakub.zmuda@gmail.com>
2024-11-24 09:18:46 +0100

    converting class to record

--- backend/src/main/java/com/github/jakubzmuda/centralControlStation/investments/domain/distributions/YearlyForecast.java
+++ backend/src/main/java/com/github/jakubzmuda/centralControlStation/investments/domain/distributions/YearlyForecast.java
@@ -3,38 +3,8 @@ package com.github.jakubzmuda.centralControlStation.investments.domain.distribut
 import com.github.jakubzmuda.centralControlStation.investments.domain.core.MultiCurrencyMonetaryValue;
 
 import java.util.List;
-import java.util.Objects;
 
-public class YearlyForecast {
-    private List<MonthlyForecast> monthlyForecasts;
-
-    public YearlyForecast(List<MonthlyForecast> monthlyForecasts) {
-        this.monthlyForecasts = monthlyForecasts;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        YearlyForecast that = (YearlyForecast) o;
-        return Objects.equals(monthlyForecasts, that.monthlyForecasts);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(monthlyForecasts);
-    }
-
-    @Override
-    public String toString() {
-        return "YearlyForecast{" +
-                "monthlyForecasts=" + monthlyForecasts +
-                '}';
-    }
-
-    public List<MonthlyForecast> monthlyForecasts() {
-        return monthlyForecasts;
-    }
+public record YearlyForecast(List<MonthlyForecast> monthlyForecasts) {
 
     public MultiCurrencyMonetaryValue total() {
         return this.monthlyForecasts
