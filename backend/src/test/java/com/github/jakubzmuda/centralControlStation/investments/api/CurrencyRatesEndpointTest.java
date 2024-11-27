de0f25db473f13ef742b2ef52307632dea2f5131
Jakub Zmuda <inbox.jakub.zmuda@gmail.com>
2024-11-27 10:24:58 +0100

    CurrencyRatesEndpoint

--- backend/src/test/java/com/github/jakubzmuda/centralControlStation/investments/api/CurrencyRatesEndpointTest.java
+++ backend/src/test/java/com/github/jakubzmuda/centralControlStation/investments/api/CurrencyRatesEndpointTest.java
@@ -3,16 +3,11 @@ package com.github.jakubzmuda.centralControlStation.investments.api;
 import com.github.jakubzmuda.centralControlStation.core.ApiTest;
 import com.github.jakubzmuda.centralControlStation.core.TestUser;
 import com.github.jakubzmuda.centralControlStation.core.rest.Response;
-import com.github.jakubzmuda.centralControlStation.investments.domain.portfolio.Portfolio;
-import com.github.jakubzmuda.centralControlStation.investments.domain.portfolio.PortfolioEntry;
 import org.junit.jupiter.api.Test;
-
-import java.util.List;
 
 import static com.github.jakubzmuda.centralControlStation.core.rest.ResponseStatus.OK;
 import static com.github.jakubzmuda.centralControlStation.core.rest.ResponseStatus.UNAUTHORIZED;
 import static com.github.jakubzmuda.centralControlStation.core.rest.RestAssertions.assertThat;
-import static org.assertj.core.api.Assertions.as;
 
 public class CurrencyRatesEndpointTest extends ApiTest {
 
