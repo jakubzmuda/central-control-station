ac8ca634c381413523e3e3434ef28bff05a96297
Jakub Żmuda <inbox.jakub.zmuda@gmail.com>
2024-11-30 17:29:26 +0100

    not found page

--- frontend/src/pages/notFoundPage/NotFoundPage.tsx
+++ frontend/src/pages/notFoundPage/NotFoundPage.tsx
@@ -8,7 +8,7 @@ function NotFoundPage() {
             <div className={styles.container}>
                 <img src={`${process.env.PUBLIC_URL}/krokiet_1.png`} alt={'confused Krokiet'} className={styles.image}/>
                 <h3>Krokiet is as confused as you are</h3>
-                <h4>Not found</h4>
+                <h4>Page not found</h4>
             </div>
         </Page>
     );
