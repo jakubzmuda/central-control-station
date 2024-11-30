1783c64744981dcd0b138f3db22d296f04bde20f
Jakub Żmuda <inbox.jakub.zmuda@gmail.com>
2024-11-30 16:37:23 +0100

    text

--- frontend/src/pages/noAccessPage/NoAccessPage.tsx
+++ frontend/src/pages/noAccessPage/NoAccessPage.tsx
@@ -7,7 +7,7 @@ function NoAccessPage() {
         <Page>
             <div className={styles.container}>
                 <img src={`${process.env.PUBLIC_URL}/truwi_1.png`} alt={'angry truwi'} className={styles.image}/>
-                <h3>Truwi spotted danger and started to is ready to inflict violence</h3>
+                <h3>Truwi spotted danger and is ready to inflict violence</h3>
                 <h4>Unrecognized user</h4>
             </div>
         </Page>
