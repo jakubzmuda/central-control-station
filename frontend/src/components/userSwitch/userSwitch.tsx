5a364e1ecd9cd349fcb3a48e471b3a7752d1a62c
Jakub Żmuda <inbox.jakub.zmuda@gmail.com>
2024-11-30 23:21:50 +0100

    user selector

--- frontend/src/components/userSwitch/userSwitch.tsx
+++ frontend/src/components/userSwitch/userSwitch.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import styles from './userSwitch.module.css';
 import {MenuItem, Select} from "@mui/material";
 
-function UserSwitch({users, selectedUser, onChange}: {users: string[], selectedUser: string, onChange: (selected: string) => void}) {
+function UserSwitch({users, selectedUser, onChange}: {
+    users: string[],
+    selectedUser: string,
+    onChange: (selected: string) => void
+}) {
     return (
         <div className={styles.container}>
             <Select
@@ -11,7 +15,15 @@ function UserSwitch({users, selectedUser, onChange}: {users: string[], selectedU
                 onChange={(event) => {
                     onChange(event.target.value);
                 }}
-                className={styles.select}
+                sx={{
+                    color: "#fff",
+                    '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
+                        borderColor: 'transparent', // Removes focus border color
+                    },
+                    '&.Mui-focused': {
+                        outline: 'none', // Removes focus outline
+                    },
+                }}
             >
                 {users.map(user => <MenuItem value={user}>{user}</MenuItem>)}
             </Select>
