d538603c62fea31c82fda246a5a10ac9159adf72
Jakub Żmuda <inbox.jakub.zmuda@gmail.com>
2024-11-30 17:28:42 +0100

    not found page

---
+++ frontend/src/pages/notFoundPage/NotFoundPage.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Page from "../../components/page/page";
+import styles from "./notFoundPage.module.css"
+
+function NotFoundPage() {
+    return (
+        <Page>
+            <div className={styles.container}>
+                <img src={`${process.env.PUBLIC_URL}/krokiet_1.png`} alt={'confused Krokiet'} className={styles.image}/>
+                <h3>Krokiet is as confused as you are</h3>
+                <h4>Not found</h4>
+            </div>
+        </Page>
+    );
+}
+
+export default NotFoundPage;
